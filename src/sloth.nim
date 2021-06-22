5585716f382575ea14cb92e87cb3f1c7abf80555
Adam Blažek <xigoi@disroot.org>
2021-06-22 23:43:44 +0200

    Lazy lists

--- src/sloth.nim
+++ src/sloth.nim
@@ -1,17 +1,41 @@
 type
   Lazy*[T] = object
-    f: proc(): T {.noSideEffect.}
-  LazyImpure*[T] = object
     f: proc(): T
+  LazyList*[T] = ref object
+    h: Lazy[T]
+    t: Lazy[LazyList[T]]
 
 template lazy*[T](x: T): Lazy[T] =
-  Lazy[T](f: func(): T = x)
+  Lazy[T](f: proc(): T = x)
 
-converter `[]`*[T](l: Lazy[T]): T =
-  l.f()
+converter `[]`*[T](x: Lazy[T]): T =
+  x.f()
 
-template lazyImpure*[T](x: T): LazyImpure[T] =
-  LazyImpure[T](f: proc(): T = x)
+proc head*[T](xs: LazyList[T]): Lazy[T] =
+  xs.h
 
-converter `[]`*[T](l: LazyImpure[T]): T =
-  l.f()
+proc tail*[T](xs: LazyList[T]): LazyList[T] =
+  xs.t
+
+template cons*[T](x: T, xs: LazyList[T]): LazyList[T] =
+  LazyList[T](h: lazy(x), t: lazy(xs))
+
+template cons*[T](x: T, xs: typeof(nil)): LazyList[T] =
+  LazyList[T](h: lazy(x), t: lazy(LazyList[T](nil)))
+
+iterator items*[T](xs: LazyList[T]): Lazy[T] =
+  var xs = xs
+  while xs != nil:
+    yield xs.head
+    xs = xs.tail
+
+proc `[]`*[T](xs: LazyList[T], i: Natural): Lazy[T] =
+  var xs = xs
+  for _ in 0..<i:
+    xs = xs.tail
+  xs.head
+
+func lazyCountup*[T](start: T): LazyList[T] =
+  cons(start, lazyCountup(start.succ))
+
+let lazyNats* = lazyCountup(0)
--- tests/test.nim
+++ tests/test.nim
@@ -1,31 +1,60 @@
 import sloth
+import std/sequtils
 import std/unittest
 
+template defineCounter() {.dirty.} =
+  var counter = 0
+  proc count[T](x: T): T =
+    inc counter
+    x
+
+template checkCount(n: int, body: untyped) =
+  counter = 0
+  body
+  check counter == n
+
 suite "lazy evaluation":
-  test "pure lazy values":
-    var plusCalled = 0
-    proc plus(a, b: int): int =
-      {.cast(noSideEffect).}:
-        plusCalled += 1
-      a + b
-    let n = plus(2, 3)
-    check plusCalled == 1
-    check n == 5
-    check plusCalled == 1
-    let l = lazy(plus(2, 3))
-    check plusCalled == 1
-    check l == 5
-    check plusCalled == 2
-  test "impure lazy values":
-    var plusCalled = 0
-    proc plus(a, b: int): int =
-      plusCalled += 1
-      a + b
-    let n = plus(2, 3)
-    check plusCalled == 1
-    check n == 5
-    check plusCalled == 1
-    let l = lazyImpure(plus(2, 3))
-    check plusCalled == 1
-    check l == 5
-    check plusCalled == 2
+  test "lazy values":
+    defineCounter
+    checkCount 1:
+      let n = count(2 + 3)
+    checkCount 0:
+      check n == 5
+    checkCount 0:
+      let l = lazy(count(2 + 3))
+    checkCount 1:
+      check l == 5
+suite "lazy lists":
+  test "construction, head, tail":
+    defineCounter
+    checkCount 0:
+      let list = cons(count(6), cons(count(2), cons(count(8), nil)))
+    checkCount 1:
+      check list.head == 6
+    checkCount 1:
+      check list.tail.head == 2
+    checkCount 1:
+      check list.tail.tail.head == 8
+  test "construction, iterating":
+    defineCounter
+    checkCount 0:
+      let list = cons(count(6), cons(count(2), cons(count(8), nil)))
+    checkCount 3:
+      check toSeq(list).mapIt(it[]) == @[6, 2, 8]
+  test "construction, indexing":
+    defineCounter
+    checkCount 0:
+      let list = cons(count(6), cons(count(2), cons(count(8), nil)))
+    checkCount 1:
+      check list[0] == 6
+    checkCount 1:
+      check list[1] == 2
+    checkCount 1:
+      check list[2] == 8
+  test "countup, indexing":
+    let c = lazyCountup(42)
+    for i in 0..100:
+      check c[i] == 42 + i
+  test "nats, indexing":
+    for i in 0..100:
+      check lazyNats[i] == i
